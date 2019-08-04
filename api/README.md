774935da0ccab998d9d39cff365cf6c81ca1c240
abs51295 <aagams68@gmail.com>
2019-08-05 00:41:11 +0530

    Modify API to perform basic CRUD operations
    - Add insert, delete, update, get and getid api
    - Render swagger spec at /api/v1/docs
    - Update README to reflect changes

--- api/README.md
+++ api/README.md
@@ -16,24 +16,28 @@ Code lies in `src/` directory. Openshift template lies in `openshift/` directory
 1. Install the requirements by running `pip install -r requirements.txt`.
 2. We are using `flask-sqlalchemy` and `flask-migrate` tools to interact with Postgres.
 3. Follow the [documentation](https://flask-migrate.readthedocs.io/en/latest/) on `flask-migrate` to initialize and run the migration command to migrate the schema. You will need to do the following sub-steps in order to migrate the DB successfully:
-    1. Point the `SQLALCHEMY_DATABASE_URI` to point to the right database URI. 
+    1. Point the `SQLALCHEMY_DATABASE_URI` to point to the right database URI. You can do that by exporting the [environment variables](src/flask_app.py#L20-22).
     2. You need to run `export FLASK_APP='src/flask_app.py'` to point to the flask application.
     3. You will need to edit `env.py` generated when you run `flask db init` in the `migrations/` directory to include all the models that we have defined. You can do that by editing the import statement to import db and edit the target metadata:
     ```Python
-    from flask_app import db
+    from models import db
     target_metadata = db.Model.metadata
     ```
-4. Once you have done the above steps you can then proceed to migrate the DB.
+4. Once you have done the above steps you can then proceed to migrate the DB by running `flask db migrate` and `flask db upgrade`.
 5. After the migration is done, initial schema is setup. You can then run the `rest_api.py` to run the API.
 
 ### How to deploy API on devcluster
 
 1. First deploy a postgres container by following the instructions [here](http://people.redhat.com/jrivera/openshift-docs_preview/openshift-origin/glusterfs-review/using_images/db_images/postgresql.html#using-images-db-images-postgresql).
-2. After the deployment of you can look at the template file located at `openshift/template.yaml` and deploy the API on dev cluster.
+2. Please edit the environment variables located at `openshift/template.yaml` to point to the right credentials for postgres and also change the image name if necessary.
+3. Now you can look at the template file located at `openshift/template.yaml` and deploy the API on dev cluster.
+4. In order to migrate the schema to your dev cluster postgres instance, please follow these steps:
+    1. Login to the dev cluster using `oc login`.
+    2. Then use port forwarding mechanism to port forward the postgres connection to your localhost. Your command will look something like `oc port-forward  postgresql-94-centos7-3-56rrw :5432`.
+    3. After that make sure to export [environment variables](src/flask_app.py#L20-22).
+    4. Then run the steps that were mentioned before.
 
-Note: You need to run `/api/v1/insert` only once to insert the data in the freshly created db instance. If you try to delete the data using `/api/v1/delete`, then again try to insert the data, it won't work. So in case you face any issues while inserting the data make sure to drop the DB and start afresh.
 
+### How to work with the API
 
-### How to check your inserted data and query the DB
-
-You can run the [API](http://openshift-probable-vulnerabilities-aagshah-fabric8-analytics.devtools-dev.ext.devshift.net/api/v1/getcve?start-date=2019-10-07&end-date=2019-10-07) to check the data that is inserted in the DB as of now. You can also just replace the call with your own API setup on your dev cluster. For more details please check the swagger spec, which you can render at [editor.swagger.io](editor.swagger.io) for a visual treat.
+You can render the [swagger specification](http://openshift-probable-vulnerabilities-aagshah-fabric8-analytics.devtools-dev.ext.devshift.net/api/v1/docs/) to see the available operations with the API. The url of the swagger spec is for the API that's hosted on my dev cluster instance. You can go to `/api/v1/docs` for your deployed API. 
--- api/src/models.py
+++ api/src/models.py
@@ -1,6 +1,14 @@
 """Models for GO CVE DB."""
 
 from flask_app import db
+from sqlalchemy.orm import validates
+from sqlalchemy.schema import CheckConstraint
+from urllib.parse import urlparse
+from datetime import timedelta, datetime, timezone
+
+
+def get_timestamp():
+    return datetime.now(tz=timezone(timedelta(), 'UTC')).isoformat(' ', 'seconds')
 
 
 class Repositories(db.Model):
@@ -9,6 +17,12 @@ class Repositories(db.Model):
     org_name = db.Column(db.String(255), nullable=False)
     name = db.Column(db.String(255), nullable=False)
     url = db.Column(db.String(255), unique=True, nullable=False)
+
+    @validates('url')
+    def validate_url(self, key, url):
+        o = urlparse(url)
+        assert o.geturl(), "Please provide valid url"
+        return url
 
 
 class Issues(db.Model):
@@ -21,10 +35,25 @@ class Issues(db.Model):
     title = db.Column(db.Text)
     repo_id = db.Column(db.Integer, db.ForeignKey('repositories.id'), nullable=False)
     url = db.Column(db.String(255), unique=True, nullable=False)
-    closed_at = db.Column(db.TIMESTAMP, nullable=False)
+    closed_at = db.Column(db.TIMESTAMP)
     is_security = db.Column(db.Integer)
     is_probable_cve = db.Column(db.Integer)
     is_passed_through_model = db.Column(db.Integer)
+
+    __table_args__ = (
+        CheckConstraint('updated_at >= created_at'),
+    )
+
+    @validates('url')
+    def validate_url(self, key, url):
+        o = urlparse(url)
+        assert o.geturl(), "Please provide valid url"
+        return url
+
+    @validates('is_security', 'is_probable_cve', 'is_passed_through_model')
+    def validate_flags(self, key, flag):
+        assert int(flag) in (0, 1), "Please provide flag as 0 or 1"
+        return flag
 
 
 class PullRequests(db.Model):
@@ -44,6 +73,21 @@ class PullRequests(db.Model):
     is_probable_cve = db.Column(db.Integer)
     is_passed_through_model = db.Column(db.Integer)
 
+    __table_args__ = (
+        CheckConstraint('updated_at >= created_at'),
+    )
+
+    @validates('url')
+    def validate_url(self, key, url):
+        o = urlparse(url)
+        assert o.geturl(), "Please provide valid url"
+        return url
+
+    @validates('is_security', 'is_probable_cve', 'is_passed_through_model')
+    def validate_flags(self, key, flag):
+        assert int(flag) in (0, 1), "Please provide flag as 0 or 1"
+        return flag
+
 
 class Commits(db.Model):
     __tablename__ = 'commits'
@@ -57,6 +101,17 @@ class Commits(db.Model):
     is_probable_cve = db.Column(db.Integer)
     is_passed_through_model = db.Column(db.Integer)
 
+    @validates('url')
+    def validate_url(self, key, url):
+        o = urlparse(url)
+        assert o.geturl(), "Please provide valid url"
+        return url
+
+    @validates('is_security', 'is_probable_cve', 'is_passed_through_model')
+    def validate_flags(self, key, flag):
+        assert int(flag) in (0, 1), "Please provide flag as 0 or 1"
+        return flag
+
 
 class ProbableCVE(db.Model):
     __tablename__ = 'probablecves'
@@ -67,11 +122,26 @@ class ProbableCVE(db.Model):
     cause_id = db.Column(db.Integer, nullable=False)
     cve_date = db.Column(db.TIMESTAMP)
     confidence_score = db.Column(db.DECIMAL(5, 2))
-    identified_date = db.Column(db.TIMESTAMP, index=True)
+    identified_date = db.Column(db.TIMESTAMP, index=True, nullable=False)
     review_comments = db.Column(db.Text)
     review_status = db.Column(db.Integer)
     is_cve = db.Column(db.Integer)
     is_triaged_internally = db.Column(db.Integer)
-    last_updated_at = db.Column(db.TIMESTAMP)
+    last_updated_at = db.Column(db.TIMESTAMP, nullable=False, onupdate=get_timestamp,
+                                default=get_timestamp)
     reviewed_by = db.Column(db.Text)
+
+    __table_args__ = (
+        CheckConstraint('last_updated_at >= identified_date'),
+    )
+
+    @validates('review_status', 'is_cve', 'is_triaged_internally')
+    def validate_flags(self, key, flag):
+        assert int(flag) in (0, 1), "Please provide flag as 0 or 1"
+        return flag
+
+    @validates('reviewed_by')
+    def validate_email(self, key, address):
+        assert '@' in address, "Please provide valid email id"
+        return address
 
